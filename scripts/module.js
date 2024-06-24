07401e0faf13018cd251a60ec941f57c1c05487a
Matheus Clemente <matheus.xb@gmail.com>
2024-06-24 01:48:12 -0300

    V12 support

--- scripts/module.js
+++ scripts/module.js
@@ -1,72 +1,59 @@
-// SPDX-FileCopyrightText: 2023 Matheus Clemente
-//
-// SPDX-License-Identifier: MIT
-
-class ImprovedPlayerList extends PlayerList {
-	_getDisplayName(user) {
-		const displayNamePart = [user.name];
-		if (user.pronouns) displayNamePart.push(`(${user.pronouns})`);
-		const flag = user.flags["improved-player-list"]?.charname;
-		if (flag) displayNamePart.push(`[${flag}]`);
-		else if (user.isGM) displayNamePart.push(`[${game.i18n.localize("USER.GM")}]`);
-		else if (user.charname) displayNamePart.push(`[${user.charname}]`);
-		return displayNamePart.join(" ");
-	}
-}
-
 Hooks.once("init", () => {
+	const origPlayerList = CONFIG.ui.players;
+	class ImprovedPlayerList extends origPlayerList {
+		_getDisplayName(user) {
+			const displayNamePart = [user.name];
+			if (user.pronouns) displayNamePart.push(`(${user.pronouns})`);
+			const flag = user.flags["improved-player-list"]?.charname;
+			if (flag) displayNamePart.push(`[${flag}]`);
+			else if (user.isGM) displayNamePart.push(`[${game.i18n.localize("USER.GM")}]`);
+			else if (user.charname) displayNamePart.push(`[${user.charname}]`);
+			return displayNamePart.join(" ");
+		}
+	}
+
 	CONFIG.ui.players = ImprovedPlayerList;
 })
 
-Hooks.once("ready", () => {
-	const userConfigList = Users.registeredSheets;
-	let userConfig = userConfigList[0];
-	if (userConfigList.length > 1) {
-		userConfig = userConfigList.find((uc) => Users.registeredSheets[0].name !== "UserConfig");
+function createForm(charname, flag) {
+	const formGroup = document.createElement("div");
+	formGroup.className = "form-group";
+
+	const label = document.createElement("label");
+	label.textContent = game.i18n.localize("Name");
+
+	const formFields = document.createElement("div");
+	formFields.className = "form-fields";
+
+	const input = document.createElement("input");
+	input.type = "text";
+	input.name = "flags.improved-player-list.charname";
+	input.placeholder = charname;
+	input.value = flag;
+
+	// Append elements
+	formFields.appendChild(input);
+	formGroup.appendChild(label);
+	formGroup.appendChild(formFields);
+
+	return formGroup;
+}
+
+Hooks.on("renderUserConfig", (userConfig, form) => {
+	const charname = userConfig.document.isGM ? game.i18n.localize("USER.GM") : userConfig.document.character?.name.split(" ")[0] || "";
+	const flag = userConfig.document.flags["improved-player-list"]?.charname ?? "";
+
+	const playerColorField = form.querySelector("color-picker[name='color']").closest(".form-group");
+	playerColorField.after(createForm(charname, flag));
+
+	if (!userConfig.document.isGM) {
+		userConfig.element.querySelector("select[name=character]").addEventListener("change", (ev) => {
+			const input = userConfig.element.querySelector(`input[name="flags.improved-player-list.charname"]`);
+			const actorId = ev.currentTarget.value;
+			const charname = game.actors.get(actorId)?.name;
+			input.placeholder = charname?.split(" ")[0] ?? "";
+		});
 	}
-
-	class ImprovedUserConfig extends userConfig {
-		// While the original getData isn't async, some system might change that (e.g. PF2e)
-		async getData(options = {}) {
-			const data = await super.getData(options);
-			const charname = this.object.isGM ? game.i18n.localize("USER.GM") : this.object.character?.name.split(" ")[0] || "";
-			const flag = this.object.flags["improved-player-list"]?.charname ?? "";
-			return {
-				...data,
-				charname,
-				flag
-			};
-		}
-
-		activateListeners(html) {
-			super.activateListeners(html);
-			if (!this.object.isGM) {
-				html.find(".actor").click((ev) => {
-					const input = html.find(`input[name="flags.improved-player-list.charname"]`);
-					const li = ev.currentTarget;
-					const actorId = li.getAttribute("data-actor-id");
-					const charname = game.actors.get(actorId)?.name;
-					input.attr("placeholder", charname.split(" ")[0]);
-				});
-			}
-		}
-	}
-
-	Users.registerSheet("improved-player-list", ImprovedUserConfig, {
-		makeDefault: true,
-		label: `Improved Player List: ${game.i18n.format("SHEETS.DefaultDocumentSheet", { document: game.i18n.localize("DOCUMENT.User") })}`
-	});
 });
 
-Hooks.on("renderUserConfig", (userConfig, html, data) => {
-	const playerColorField = html.find(".form-group input[name='color']").closest(".form-group");
-	playerColorField.after(
-		`<div class="form-group">
-			<label>${game.i18n.localize("Name")}</label>
-			<div class="form-fields">
-				<input type="text" name="flags.improved-player-list.charname" placeholder="${data.charname}" value="${data.flag}">
-			</div>
-		</div>`
-		);
-	userConfig.setPosition();
-});
+Hooks.on("closeUserConfig", () => ui.players.render(true));
