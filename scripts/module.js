6c15de6e466cb96648239c26a8d3241baf1e8844
Matheus Clemente <matheus.xb@gmail.com>
2025-10-05 18:44:00 -0300

    Update module.js

--- scripts/module.js
+++ scripts/module.js
@@ -2,13 +2,13 @@ Hooks.once("init", () => {
 	const origPlayerList = CONFIG.ui.players;
 	class ImprovedPlayerList extends origPlayerList {
 		_formatName(user) {
-			const displayNamePart = [user.name];
-			if (user.pronouns) displayNamePart.push(`(${user.pronouns})`);
+			const parts = [user.name];
+			if (user.pronouns) parts.push(`(${user.pronouns})`);
 			const flag = user.flags["improved-player-list"]?.charname;
-			if (flag) displayNamePart.push(`[${flag}]`);
-			else if (user.isGM) displayNamePart.push(`[${game.i18n.localize("USER.GM")}]`);
-			else if (user.charname) displayNamePart.push(`[${user.character.name}]`);
-			return displayNamePart.join(" ");
+			if (flag) parts.push(`[${flag}]`);
+			else if (user.isGM) parts.push(`[${game.i18n.localize("USER.GM")}]`);
+			else if (user.character) parts.push(`[${user.character.name}]`);
+			return parts.join(" ");
 		}
 	}
 
@@ -45,20 +45,21 @@ function createForm(charname, flag) {
 }
 
 Hooks.on("renderUserConfig", (userConfig, form) => {
-	const charname = userConfig.document.isGM ? game.i18n.localize("USER.GM") : userConfig.document.character?.name.split(" ")[0] || "";
-	const flag = userConfig.document.flags["improved-player-list"]?.charname ?? "";
+	const { document: doc, element } = userConfig;
+	const charname = doc.isGM ? game.i18n.localize("USER.GM") : (doc.character?.name ?? "");
+	const flag = doc.flags["improved-player-list"]?.charname ?? "";
 
 	const playerColorField = form.querySelector("color-picker[name='color']").closest(".form-group");
 	playerColorField.after(createForm(charname, flag));
 
-	if (!userConfig.document.isGM) {
-		userConfig.element.querySelector("select[name=character]").addEventListener("change", (ev) => {
-			const input = userConfig.element.querySelector(`input[name="flags.improved-player-list.charname"]`);
+	if (!doc.isGM) {
+		const input = element.querySelector(`input[name="flags.improved-player-list.charname"]`);
+		element.querySelector("select[name=character]").addEventListener("change", (ev) => {
 			const actorId = ev.currentTarget.value;
-			const charname = game.actors.get(actorId)?.name;
-			input.placeholder = charname?.split(" ")[0] ?? "";
+			input.placeholder = game.actors.get(actorId)?.name ?? "";
+		});
+		element.querySelector("button[data-action=releaseCharacter]")?.addEventListener("click", (ev) => {
+			input.placeholder = "";
 		});
 	}
 });
-
-Hooks.on("closeUserConfig", () => ui.players.render(true));
